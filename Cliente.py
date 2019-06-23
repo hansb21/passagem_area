1e80daee034784d4e279a6deb63e9531420f3ca7
hansb21 <hans.buss@mailfence.com>
2019-06-23 17:58:46 -0300

    Add files via upload

--- Cliente.py
+++ Cliente.py
@@ -3,10 +3,8 @@ class Cliente:
         self.nome = nome
         self.cpf  = cpf
         self.voosComprados = []
-    
-    # def __init__(self):
-    #     self.__dict = {}
-    
-    # def __len__(self):
-    #     return len()
-    
+
+    def removerVoo(self, vooPassagemCancelada):
+        for num, cont in enumerate(self.voosComprados):
+            if cont(1) == vooPassagemCancelada:
+                self.voosComprados.pop(num)
--- Voos.py
+++ Voos.py
@@ -5,10 +5,33 @@ class Voos:
         self.numeroDeVoo = num
 
         self.numeroMaximoPassageiros = 10
+        self.passageiros = []
+        self.quantidadeDePassagens = []
         self.passageirosAtuais = []
 
+    def removerPassageiro(self, passageiro):
+        for num, cont in enumerate(self.passageiros):
+            if cont == passageiro:
+                self.passageiros.pop(num)
+                self.quantidadeDePassagens.pop(num)
 # class Voos:
-    
+
 #     def __init__(self):
 #         self.__dbvoo = []
 
+    def getNumPassageiros():
+        return sum(quantidadeDePassagens)
+
+    def reservarPassagem(self, cliente):
+        print('Tipo: Avião Fretado')
+        numPassagens = int(input('Quantas passagens deseja reservar?\n'))
+        preco = numPassagens*100
+
+        resposta = input(f'o preço da passagem é {preco} \ndeseja continuar? \n')
+
+        if resposta == 's':
+            self.passageiros.append(cliente)
+            self.quantidadeDePassagens.append(numPassagens)
+            cliente.voosComprados.append(self)
+        else:
+            menuConsultar()
