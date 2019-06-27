787872fb640604dca24f5acf80c06b976b42041c
hansb21 <hans.buss@mailfence.com>
2019-06-26 21:34:54 -0300

    Add files via upload

--- interfaces.py
+++ interfaces.py
@@ -1,5 +1,13 @@
-from termcolor import colored
 import os
+import time
+from acessoCliente import *
+from acessoOperador import *
+from colorama import init
+from termcolor import colored
+
+from reservas import *
+from Cliente import *
+
 
 def mostrarApresentacao():
     os.system('clear')
@@ -9,6 +17,29 @@ def mostrarApresentacao():
     print('*' * 51)
     input()
 
+def loginOperador():
+    user  = input('Usuário: ')
+    senha = input('Senha: ')
+    os.system('clear')
+
+    if user == 'admin' and senha == 'admin':
+        menuOperador()
+
+def loginCliente():
+    nome = input('Nome: ')
+    
+    cpf  = input('CPF: ')
+    os.system('clear')
+
+    cliente = acharCliente(nome, cpf)
+
+    if not cliente:
+        cliente = Cliente(nome.title(), cpf)
+        novoCliente(cliente)
+        os.system('clear')
+
+
+    menuCliente(cliente)
 def mostrarMenuPrincipal():
     os.system('clear')
 
@@ -18,3 +49,29 @@ def mostrarMenuPrincipal():
     print(colored('2 - Sair                  ', 'green'))
     print()
     print(colored('Como você deseja acessar: ', 'green'), end = '')
+def escolhaMenu(n):
+    if n == 0:
+        os.system('clear')
+
+        print('*' * 51)
+        print(' ')
+        print(colored('0 - Operador', 'red'))
+        print(colored('1 - Cliente', 'green'))
+        print(colored('2 - Sair','green'))            
+        print(' ')
+        time.sleep(1)
+        loginOperador()
+        
+    elif n == 1:
+        os.system('clear')
+        print('*' * 51)
+        print(' ')
+        print(colored('0 - Operador', 'green'))
+        print(colored('1 - Cliente', 'red'))
+        print(colored('2 - Sair','green'))    
+        print(' ')
+        time.sleep(1)
+        loginCliente()
+        
+
+        
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-
+import time
 from acessoCliente import *
 from acessoOperador import *
 from colorama import init
@@ -8,29 +8,7 @@ from interfaces import *
 from reservas import *
 from Cliente import *
 
-def loginOperador():
-    user  = input('Usuário: ')
-    senha = input('Senha: ')
-    os.system('clear')
 
-    if user == 'admin' and senha == 'admin':
-        menuOperador()
-
-def loginCliente():
-    nome = input('Nome: ')
-    
-    cpf  = input('CPF: ')
-    os.system('clear')
-
-    cliente = acharCliente(nome, cpf)
-
-    if not cliente:
-        cliente = Cliente(nome.title(), cpf)
-        novoCliente(cliente)
-        os.system('clear')
-
-
-    menuCliente(cliente)
 
 #Começa aqui
 mostrarApresentacao()
@@ -38,15 +16,11 @@ mostrarApresentacao()
 while True:
     mostrarMenuPrincipal()
     escolha_Login = int(input())
-    
-    os.system('clear')
-
-    if escolha_Login == 0:
-        loginOperador()
-    elif escolha_Login == 1:
-        loginCliente()
-    elif escolha_Login == 2:
+    if escolha_Login == 2:
         break
+    else:
+        escolhaMenu(escolha_Login)
+    
 
 
 
